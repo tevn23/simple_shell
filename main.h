d48bf4b9bba2da6afba28b39195ce13ebf071288
tevn23 <mabulatt23@gmail.com>
2023-11-15 14:11:52 +0200

    Task 1 final fix

--- main.h
+++ main.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-#endif
+#define MAX_AV 64
+
+#endif /* MAIN_H */
